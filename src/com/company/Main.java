a1adc1d86f57d3e286624ee1193634ae39bd4bec
jroberts3136 <Jroberts3136@gmail.com>
2017-10-10 08:45:46 -0400

    Finished base TWIP

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,40 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner keyInput = new Scanner(System.in);
+        Scanner stringInput = new Scanner(System.in);
+
+        boolean done = false;
+        boolean promptAgain;
+        while(!done){
+            System.out.println("What is your name?");
+            String name = stringInput.nextLine();
+            perfectRandom user = new perfectRandom(name);
+            int luckyNum = user.perfectSquare();
+            System.out.println("Nice to meet you " + user.name + "! Your lucky number is " + luckyNum + ".");
+
+            promptAgain = true;
+
+            while(promptAgain){
+                System.out.println("Press 0 to exit. Press 1 to display your name and number again. Press 2 to enter a new name and receive a new number.");
+                int response = keyInput.nextInt();
+                switch (response){
+                    case 0:
+                        promptAgain = false;        //Program finishes
+                        done = true;
+                        break;
+                    case 1:
+                        System.out.println("Nice to meet you " + name + "! Your lucky number is " + luckyNum + ".");    //Prints, then prompts again
+                        break;
+                    case 2:
+                        promptAgain = false;        //Starts from beginning
+                        break;
+                }
+            }
+        }
     }
 }
--- src/com/company/perfectRandom.java
+++ src/com/company/perfectRandom.java
@@ -4,4 +4,17 @@ package com.company;
  * Created by jroberts3136 on 10/10/17.
  */
 public class perfectRandom {
+    public String name;
+    private int rawRandom;
+    private int perfectRandom;
+
+    public perfectRandom(String n){
+        name = n;                   //takes user's name
+    }
+
+    public int perfectSquare(){
+        rawRandom = (int)(Math.random() * 101); //Generates random integer from 0 to 100, inclusive of both
+        perfectRandom = (int)Math.pow(rawRandom, 2);    //Squares integer -> Results in all perfect squares from 0 to 10000
+        return perfectRandom;
+    }
 }
